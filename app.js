253e5fb0800cc1269c206fe2b0ffa990baac8c4c
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-08 12:49:52 -0400

    Viendo typeof

--- app.js
+++ app.js
@@ -2,17 +2,27 @@
 
 function identifica( param ){
 
-    console.log( typeof param);  // Esto imprime el tipo de parametro que le pases a la funcion
+    // console.log( typeof param);  // Esto imprime el tipo de parametro que le pases a la funcion
 
+    if( typeof param === "function" ){
+
+        param();
+    }
+    else{
+
+        console.log( param );
+
+    }
 }
 
 function Persona(){
 
-
+    this.nombre = "Rodrigo";
+    this.edad = 20;
 
 }
 
-var juan = new Persona();
+var rodrigo = new Persona();
 
 
-identifica( function(){} );  // Se pueden mandar obj, funciones, numers, boolean, string, etc.
+identifica( rodrigo );  // Se pueden mandar obj, funciones, numbers, boolean, string, etc.
