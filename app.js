293d43937f2ab9da6caef074468b598d7935010e
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-08 01:32:09 -0400

    Viendo funciones anonimas

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
-function Persona() {
 
-    this.nombre = "Rodrigo";
-    this.apellido = "Salinas";
-    this.edad = 20;
-    this.pais = "Chile";
+( function(){    // Esto es una funcion anonima.
 
-}
+    var a = 10;
 
-Persona.prototype.imprimirInfo = function () {
+    console.log( a );
 
-    this.imprimirInfo = function () {
+    function cambiarA(){
 
-        console.log( this.nombre + " " + this.apellido + " (" + this.edad +")" );
+        a = 20;
 
     }
 
-}
+    cambiarA();
 
-// Persona.prototype.pais = "Chile"; Guardando una propiedad dentro del prototipo
+    console.log( a );
 
-var rod = new Persona();  // Esto crea una nueva instancia de "Persona".
+} )();
 
-/* console.log(rod);
-   console.log(rod.pais);  Cuando JS busca una propiedad que no esta dentro del constructor la busca en los prototipos y si la encuentra, la trae */
-
-// rod.imprimirInfo();  Esto es para llamar al metodo a traves de la variable "rod".
-
-console.log(rod);
-console.log(rod.imprimirInfo());
-
-Number.prototype.esPositivo = function(){
-
-    if (this > 0) {
-    
-        return true;
-
-    }
-    else{
-
-        return false;
-
-    }
-
-}
