44c9b38f3147a4fbd079805719f76de6f240ec03
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-08 17:29:43 -0400

    Terminando de ver argumentos

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var arguments = 10;
 
 function miFuncion(a, b, c, d){
 
-    console.log( arguments );
+    if( arguments.length !== 4 ){   // Si la funcion es distinta a 4 se ejecuta lo que tiene dentro.
+
+        console.error( "La fucion necesita 4 parametros!" );
+        return;
+
+    }
+
+    console.log( a + b + c + d );
 
 }
 
-miFuncion(10);
+miFuncion(10, 20, 30, 40, 50, 60);  // Si mandamos algo distinto a 4 argumentos la funcion no se ejecutara
