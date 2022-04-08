996cde661df738d52a557b2789b0e052c333d6e9
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-08 12:29:18 -0400

    Viendo funciones typeof e instanceof

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-// Las funciones anonimas se crean al momento de invocar a una funcion, que esta pidiendo otra funcion (esta seria la anonima) y no guardan nada dentro del objeto global window.
 
-function ejecutarFuncion( fn ){
 
-    if( fn() === 1 ){
+function identifica( param ){
 
-        return true;
-        
-    }
-    else{
-
-        return false;
-
-    }
-
+    console.log( typeof param);
 
 }
 
-console.log( 
 
-    ejecutarFuncion( function(){   // Esto es una funcion anonima
-    
-        console.log( "Estoy funcionando :D" );
-        return 1;
-    
-    } )
-
- );
-
+identifica( 1 );
