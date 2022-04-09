c5fdd1be73b67f842ea9747a5db959f3107d0978
Rodrigo <rodrigo.salinas23@hotmail.com>
2022-04-08 21:53:27 -0400

    Terminando TypeScript

--- TypeScript/salida.js
+++ TypeScript/salida.js
@@ -1,4 +1,19 @@
-// TypeSctipt convierte codigo desde .ts a .js, para que pueda ser soportado en casi cualquier entorno
+// TypeSctipt convierte codigo desde .ts a .js, para que pueda ser soportado en casi cualquier entorno. Esta casi totalmente orientado a objetos 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("Hola mundo");
 // types
 var miString = "Hola mundo";
@@ -8,4 +23,75 @@ var miBoolean = true || false;
 var miVar = "Hola"; // Con :any acepta cualquier tipo de dato para despues ser cambiado. En js se puede hacer este cambio sin necesidad de aplicar esto.
 miVar = false;
 // Strings
-document.write(miNumero.toString()); // Esto sirve para imprimir por pantalla (Sale en el documento).  document.write escribe tipo string, por lo tanto se debe convertir.
+// document.write(miNumero.toString());  // Esto sirve para imprimir por pantalla (Sale en el documento).  document.write escribe tipo string, por lo tanto se debe convertir.
+// Arrays
+var miArray = ["item1", "item2", ""]; // Distintos tipos de arrays
+var miArrayNumber = [1, 2, 3, 4];
+var miArrayBoolean = [true, false];
+var miArrayAny = ["Hola", true, 2];
+//Tupls   // Esto determina cierto orden de tipo de datos en un arreglo, cuando se definan que llevaran deben seguir el orden
+var strNumTupl;
+strNumTupl = ["Hello", 2];
+strNumTupl = ["World", 22];
+// void, undefined, null
+// "let" Es como un var, pero no pierde el alcance. "const" tambien es como un var pero constante, es decir no se le puede asignar un nuevo valor.
+// let miVoid: void = undefined;
+// let miNull: null = null;
+// let miUndefined: undefined = undefined; 
+// Functions
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+var miSum = function (num1, num2) {
+    if (typeof (num1) === "string") {
+        num1 = parseInt(num1);
+    }
+    if (typeof (num2) === "string") {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+function showToDo(todo) {
+    console.log(todo.tittle + " - " + todo.text);
+}
+var myTodo = { tittle: "Eat Dinner", text: "lorem" };
+showToDo(myTodo);
+document.write();
+// Clases
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, email, edad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+    Usuario.prototype.register = function () {
+        console.log(this.nombre + " Se ha registrado con exito");
+    };
+    Usuario.prototype.showMeAge = function () {
+        return this.edad;
+    };
+    Usuario.prototype.ageInYears = function () {
+        return this.edad + " Años";
+    };
+    Usuario.prototype.payInvoice = function () {
+        console.log(this.nombre + " Ha realizado el pago con exito!");
+    };
+    return Usuario;
+}());
+var rodrigo = new Usuario("Rodrigo", "rodrigo@email.com", 20);
+console.log(rodrigo.ageInYears());
+var Member = /** @class */ (function (_super) {
+    __extends(Member, _super);
+    function Member(id, nombre, email, edad) {
+        var _this = _super.call(this, nombre, email, edad) || this;
+        _this.id = id;
+        return _this;
+    }
+    Member.prototype.payInvoice = function () {
+        _super.prototype.payInvoice.call(this); // Esto esta heredando el metodo "payInvoice".
+    };
+    return Member;
+}(Usuario));
+var gordon = new Member(1, "Gordon", "gordon@email.com", 33);
+gordon.payInvoice();
+document.write();
--- TypeScript/salida.ts
+++ TypeScript/salida.ts
@@ -1,4 +1,4 @@
-// TypeSctipt convierte codigo desde .ts a .js, para que pueda ser soportado en casi cualquier entorno
+// TypeSctipt convierte codigo desde .ts a .js, para que pueda ser soportado en casi cualquier entorno. Esta casi totalmente orientado a objetos 
 
 console.log("Hola mundo");
 
@@ -19,4 +19,153 @@ miVar = false;
 
 // Strings
 
-document.write(miNumero.toString());  // Esto sirve para imprimir por pantalla (Sale en el documento).  document.write escribe tipo string, por lo tanto se debe convertir.
+// document.write(miNumero.toString());  // Esto sirve para imprimir por pantalla (Sale en el documento).  document.write escribe tipo string, por lo tanto se debe convertir.
+
+// Arrays
+
+var miArray: string[] = ["item1", "item2", ""];   // Distintos tipos de arrays
+var miArrayNumber: number[] = [1, 2, 3, 4];
+var miArrayBoolean: boolean[] = [true, false];
+var miArrayAny: any[] = ["Hola", true, 2];
+
+//Tupls   // Esto determina cierto orden de tipo de datos en un arreglo, cuando se definan que llevaran deben seguir el orden
+
+var strNumTupl: [string, number];
+
+strNumTupl = ["Hello", 2];
+strNumTupl = ["World", 22];
+
+// void, undefined, null
+// "let" Es como un var, pero no pierde el alcance. "const" tambien es como un var pero constante, es decir no se le puede asignar un nuevo valor.
+
+
+// let miVoid: void = undefined;
+// let miNull: null = null;
+// let miUndefined: undefined = undefined; 
+
+// Functions
+
+function getSum( num1: number, num2: number ): number{
+
+    return num1 + num2;
+
+}
+
+let miSum = function( num1: number | string, num2: number | string ): number {
+    
+    if( typeof(num1) === "string" ){
+
+        num1 = parseInt(num1);
+
+    }
+    if( typeof(num2) === "string" ){
+
+        num2 = parseInt(num2);
+
+    }
+
+    return num1 + num2;
+
+};
+
+// function getName( fName: string, lName?: string ){   // El simbolo "?" quiere decir que es un parametro opcional
+
+//     if(lName === undefined){
+//         return fName;
+//     }
+
+//     return fName + " " + lName;
+
+// }
+
+// document.write(getName("Rodrigo", "Salinas"));  // Esto esta imprimendo y pasa datos (fName y lName)
+
+
+
+// function myVoidF(): void{
+
+//     return ;
+
+// }
+
+// Interfaces
+
+interface Itodo{    // Esta es la estructura de la interface
+    tittle: string;
+    text: string;
+}
+
+function showToDo(todo: Itodo ){    // Las interfaces entregan la estructura de un tipo de dato, esto le esta diciendo que esta funcion tiene una interfaz llamada Itodo.
+
+    console.log( todo.tittle + " - " + todo.text );
+
+}
+
+let myTodo: Itodo = { tittle: "Eat Dinner", text: "lorem" };
+
+showToDo(myTodo);
+
+document.write();
+
+// Clases
+
+class Usuario {  // Esto es una clase 
+
+    private nombre: string;
+    public email: string;
+    protected edad: number;
+
+    constructor( nombre: string, email: string, edad: number ) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    register(){
+        console.log( this.nombre + " Se ha registrado con exito" );
+    }
+
+    showMeAge(){
+
+        return this.edad;
+
+    }
+
+    public ageInYears(){
+        return this.edad + " Años";
+
+    }
+
+    payInvoice(){
+
+        console.log( this.nombre + " Ha realizado el pago con exito!" );
+        
+    }
+
+}
+
+var rodrigo = new Usuario( "Rodrigo", "rodrigo@email.com", 20 );
+
+console.log( rodrigo.ageInYears() );
+
+class Member extends Usuario{  // Esto esta recogiendo todos los metodos y parametros de la clase "Usuario".
+
+    id: number;
+
+    constructor( id, nombre, email, edad ){
+        super( nombre, email, edad );  // Esto hereda lo quie le indiquemos de la clase padre.
+
+        this.id = id;
+
+    }
+
+    payInvoice(){
+        super.payInvoice();  // Esto esta heredando el metodo "payInvoice".
+    }
+}
+
+var gordon = new Member( 1, "Gordon", "gordon@email.com", 33 );
+
+gordon.payInvoice();
+
+document.write(  );
